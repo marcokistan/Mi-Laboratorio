1eef17149feda5d75f41a9f51a2df164a0b6295f
Marcokistan <marcorgez07@gmail.com>
2026-06-11 23:22:59 +0100

    Creado el proyecto de ejercicio: Ordenar Array
    Creacion del proyecto.

---
+++ Apuntes y Programas/Java/OrdenarArray/src/App.java
@@ -0,0 +1,7 @@
+// Programa muy básico, en donde vamos a ordenar la lista de un usuario.
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
